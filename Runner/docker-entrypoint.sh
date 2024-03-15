52babbc8c99a531f26d5c0ba59858de9114dafff
Miha Zupan <mihazupan.zupan1@gmail.com>
2024-03-16 00:51:13 +0100

    Fix docker build

--- Runner/docker-entrypoint.sh
+++ Runner/docker-entrypoint.sh
@@ -1,3 +1,3 @@
-﻿git clone --no-tags --single-branch --progress https://github.com/MihaZupan/runtime-utils
+git clone --no-tags --single-branch --progress https://github.com/MihaZupan/runtime-utils
 cd runtime-utils/Runner
 dotnet run -c Release
